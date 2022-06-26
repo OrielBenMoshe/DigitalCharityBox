c4b806e54cb3453f05f62b28bf0e4c89e38c3839
nerisellem <94001095+nerisellem@users.noreply.github.com>
2022-06-26 21:49:15 +0300

    new icon

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,52 +1,46 @@
-import React, { useState } from 'react';
-import { Button, Drawer } from 'antd';
-import { MenuOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import { Button, Drawer } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
 
-import Settings from './Settings';
+import Settings from "./Settings";
 
 function Header() {
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const showMenu = () => {
-        setVisible(true);
-    };
+  const showMenu = () => {
+    setVisible(true);
+  };
 
-    const onClose = () => {
-        setVisible(false);
-    };
+  const onClose = () => {
+    setVisible(false);
+  };
 
-    const showSettings = () => {
-        
-    } ;
+  const showSettings = () => {};
 
-    const menu = (
-        <Drawer 
-            title="הגדרות" 
-            placement="right" 
-            onClose={onClose} 
-            visible={visible} 
-            width="60%"
-        >
-            <Settings title={"פרטים אישיים"}/>
-            <Settings title={"הגדרות תצוגה"}/>
-            <Settings title={"פרטי כרטיס אשראי"}/>
-            <Settings title={"הגדרות תזכורת"}/>
-            {/* <p>פרטי כרטיס אשראי</p>
-            <p>עריכת פרטים אישיים</p>
-            <p>הגדרות תצוגה</p>
-            <p>הגדרות נוספות</p> */}
-        </Drawer>
-    );
+  const menu = (
+    <Drawer
+      title="הגדרות"
+      placement="right"
+      onClose={onClose}
+      visible={visible}
+      width="60%"
+    >
+      <Settings title={"ניהול תזכורת"} />
+      <Settings title={"פרטים אישיים"} />
+      <Settings title={"הגדרות תצוגה"} />
+      <Settings title={"פרטי כרטיס אשראי"} />
+    </Drawer>
+  );
 
-    return (
-        <div className="Header">
-            <Button type="text" onClick={showMenu}>
-                <MenuOutlined />
-            </Button>
-            {menu}
-            <h2>קופת צדקה דיגיטלית</h2>
-        </div>
-    )
+  return (
+    <div className="Header">
+      <Button type="text" onClick={showMenu}>
+        <MenuOutlined />
+      </Button>
+      {menu}
+      <h2>קופת צדקה דיגיטלית</h2>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
