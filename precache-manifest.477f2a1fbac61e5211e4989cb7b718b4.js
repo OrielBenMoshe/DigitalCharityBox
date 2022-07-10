b6e86a1bba8b89b46d12fe13bdccf3473b94c022
OrielBenMoshe <ori.l.2m@gmail.com>
2022-07-10 11:58:26 +0300

    Updates

---
+++ precache-manifest.477f2a1fbac61e5211e4989cb7b718b4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f41818b9788d397d255a",
+    "url": "/DigitalCharityBox/static/css/main.7901beb4.chunk.css"
+  },
+  {
+    "revision": "f41818b9788d397d255a",
+    "url": "/DigitalCharityBox/static/js/main.3e8af764.chunk.js"
+  },
+  {
+    "revision": "c435f99e621ec1603fb1",
+    "url": "/DigitalCharityBox/static/js/runtime~main.106c7463.js"
+  },
+  {
+    "revision": "293c67c077a79ff1a2e1",
+    "url": "/DigitalCharityBox/static/css/2.33664958.chunk.css"
+  },
+  {
+    "revision": "293c67c077a79ff1a2e1",
+    "url": "/DigitalCharityBox/static/js/2.92f20d44.chunk.js"
+  },
+  {
+    "revision": "5c1378a104c6a828198d",
+    "url": "/DigitalCharityBox/static/js/3.21ec2f43.chunk.js"
+  },
+  {
+    "revision": "0f08b2ee9a65cedb6efc",
+    "url": "/DigitalCharityBox/static/js/4.c7197477.chunk.js"
+  },
+  {
+    "revision": "02391add6cb0cd324ddc5ee870a9c7b6",
+    "url": "/DigitalCharityBox/static/media/eserShekel_tails.02391add.svg"
+  },
+  {
+    "revision": "737ac4a4d94e8bb37bb05021d306e693",
+    "url": "/DigitalCharityBox/static/media/shekel_tails.737ac4a4.svg"
+  },
+  {
+    "revision": "a4d8ad97ede4c94223b0e931add31e4a",
+    "url": "/DigitalCharityBox/static/media/shnekel_tails.a4d8ad97.svg"
+  },
+  {
+    "revision": "c032bbf86e95b35c83342d063a8429ba",
+    "url": "/DigitalCharityBox/static/media/hameshShekel_tails.c032bbf8.svg"
+  },
+  {
+    "revision": "fd7d5901b04186e8ba23ae992ff1d27b",
+    "url": "/DigitalCharityBox/static/media/charity_box.fd7d5901.png"
+  },
+  {
+    "revision": "069eae1caee888ac6fa215367ce6e7f6",
+    "url": "/DigitalCharityBox/static/media/NotoSerifHebrew-Black.069eae1c.ttf"
+  },
+  {
+    "revision": "3039ac8f9e9406e52d1673ea914870c4",
+    "url": "/DigitalCharityBox/static/media/NotoSerifHebrew-SemiBold.3039ac8f.ttf"
+  },
+  {
+    "revision": "5b148a6652fdbbaa1605cf7e83057bcd",
+    "url": "/DigitalCharityBox/static/media/NotoSerifHebrew-Light.5b148a66.ttf"
+  },
+  {
+    "revision": "b82c5a7cbd6d8478fcc743c94a8362fc",
+    "url": "/DigitalCharityBox/static/media/NotoSerifHebrew-Thin.b82c5a7c.ttf"
+  },
+  {
+    "revision": "dc19c20043718c182c8fdab003e37f1b",
+    "url": "/DigitalCharityBox/index.html"
+  }
+];
